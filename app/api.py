9acdd9d6a385d210edc4dd70a00674483becb521
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-03-07 22:33:53 -0800

    Added Clinical data route

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.getClinicalData import getClinicalData
 
 app = FastAPI()
 
@@ -22,3 +22,11 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to the RHAIN server"}
+
+
+@app.get("/county-info")
+def get_county_info(county_name: str):
+    # Filter the rows by county
+    data = getClinicalData(county_name)
+    # Convert the selected data to a dictionary and return it
+    return data.to_dict(orient='records')
---
+++ app/getClinicalData.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def getClinicalData(county_name):
+    # Read the Excel file into a pandas dataframe
+    df = pd.read_excel('app/data/Clinical_Care_CA.xlsx')    
+
+    # Filter the rows by county
+    county_df = df.loc[df['County'] == county_name]
+
+    # Select the columns you want
+    selected_columns = ['Primary Care Physicians Ratio Formatted', 'Dentist Ratio Formatted', 'Mental Health Provider Ratio Formatted']
+    selected_df = county_df[selected_columns]
+
+    # Print the selected data
+    return selected_df
+    # print(selected_df)
