5e03aa228f49aafe93f5c138b3486d7fa347a22a
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-03-11 01:44:19 -0800

    Improved performance of getClinicalData

--- app/getClinicalData.py
+++ app/getClinicalData.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
+data_path_clinical_data = os.path.join(os.getcwd(), 'app/data/2022 County Health Rankings Data.xlsx')
+clinical_data_excel = pd.read_excel(data_path_clinical_data ,sheet_name = 3,header=1)
+
 def convertRatio (x):
     if x is np.nan:
         return np.nan
@@ -10,17 +13,17 @@ def convertRatio (x):
     return c
 
 def getClinicalData(state_name, county_name):
-
-    # Read the Excel file into a pandas dataframe
-    data_path = os.path.join(os.getcwd(), 'app/data/2022 County Health Rankings Data.xlsx')
-    xl = pd.read_excel(data_path ,sheet_name = 3,header=1)
-    selected_df = xl[["State","County","Primary Care Physicians Ratio","Dentist Ratio","Mental Health Provider Ratio"]]
-
+    print("Reading", state_name, county_name)
+    # Read the Excel file into a pandas dataframe    
+    # print("p1")
+    selected_df = clinical_data_excel[["State","County","Primary Care Physicians Ratio","Dentist Ratio","Mental Health Provider Ratio"]]
+    # print("p2")
     selected_df = selected_df.loc[(selected_df['State'] == state_name) & (selected_df['County'] == county_name)]
     del selected_df[selected_df.columns.values[0]]
     del selected_df[selected_df.columns.values[0]]
-    
+    # print("p3")
     for col in ["Primary Care Physicians Ratio","Dentist Ratio","Mental Health Provider Ratio"]:
         selected_df[col] = selected_df[col].apply(convertRatio)
+    print("Done")
     return selected_df
 
