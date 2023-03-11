bdbdd2e708a6ac791847c9921960103927460a81
Sangeeta Rao <sangeeta@Sangeetas-MacBook-Pro.local>
2023-03-11 00:05:27 -0800

    Made changes to get ClinicalData from main excel file  and clean it

--- app/getClinicalData.py
+++ app/getClinicalData.py
@@ -1,16 +1,26 @@
 import pandas as pd
+import numpy as np
+import os
+
+def convertRatio (x):
+    if x is np.nan:
+        return np.nan
+    a, b = x.split(':') 
+    c = int(a)/int(b) 
+    return c 
 
 def getClinicalData(state_name, county_name):
+
     # Read the Excel file into a pandas dataframe
-    df = pd.read_excel('app/data/Clinical_Care_CA.xlsx')    
+    data_path = os.path.join(os.getcwd(), 'app/data/2022 County Health Rankings Data.xlsx')
+    xl = pd.read_excel(data_path ,sheet_name = 3,header=1)
+    selected_df = xl[["State","County","Primary Care Physicians Ratio","Dentist Ratio","Mental Health Provider Ratio"]]
 
-    # Filter the rows by county
-    county_df = df.loc[df['County'] == county_name]
-
-    # Select the columns you want
-    selected_columns = ['Primary Care Physicians Ratio Formatted', 'Dentist Ratio Formatted', 'Mental Health Provider Ratio Formatted']
-    selected_df = county_df[selected_columns]
-
-    # Print the selected data
+    selected_df = selected_df.loc[(selected_df['State'] == state_name) & (selected_df['County'] == county_name)]
+    del selected_df[selected_df.columns.values[0]]
+    del selected_df[selected_df.columns.values[0]]
+    
+    for col in ["Primary Care Physicians Ratio","Dentist Ratio","Mental Health Provider Ratio"]:
+        selected_df[col] = selected_df[col].apply(convertRatio)
     return selected_df
-    # print(selected_df)
+
