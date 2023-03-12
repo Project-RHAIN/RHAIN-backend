7b21a3ac0905a1a797349c14fb50ab7a6c12d785
Rithesh <keatz68@gmail.com>
2023-03-11 16:13:02 -0800

    PerceptionScore updated

--- app/PerceptionScore.py
+++ app/PerceptionScore.py
@@ -5,10 +5,16 @@ import random
 import pandas as pd
 from random import sample
 import os
+import csv
 
 
 data_path_reviews = os.path.join(os.getcwd(), 'app\data\BackendReviews.csv')
+data_path_scores = os.path.join(os.getcwd(), 'app\data\Scores.csv')
 data_path_model = os.path.join(os.getcwd(), 'app\DistilBert')
+
+with open(data_path_scores, newline='') as csv_file:
+    reader = csv.DictReader(csv_file)
+    rows = {(row['State'], row['County']): row['Score'] for row in reader}
 
 df = pd.read_csv(data_path_reviews)
 dbModel = tf.keras.models.load_model(data_path_model)
@@ -36,11 +42,24 @@ def score_compute(reviews, model, tokenizer):
         return np.nan
 
 
+# def get_sentiment_score(state_name, county_name):
+#     reviews = get_reviews(county_name)
+#     score = round((score_compute(reviews, dbModel, dbTokenizer) + 1) * 5, 2)
+#     return {"Perception score": score}
+
+def get_score(state_name, county_name):
+    score = rows.get((state_name, county_name))
+    if score != 'nan':
+        return score
+    else:
+        return -1
+
+
 def get_sentiment_score(state_name, county_name):
-    reviews = get_reviews(county_name)
-    score = round((score_compute(reviews, dbModel, dbTokenizer) + 1) * 5, 2)
-    return {"Perception score": score}
-
-
+    score = get_score(state_name, county_name)
+    if score != -1:
+        return {"Perception score": score}
+    else:
+        return {"No reviews available for scoring"}
 
 
