d9ba71fd9a63dfbeb9d6fbc1f3d32567269c5aa6
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-06-08 18:30:17 -0700

    Lean for prod

--- app/PerceptionScore.py
+++ app/PerceptionScore.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
-from transformers import DistilBertTokenizer
+# from transformers import DistilBertTokenizer
 import random
 import pandas as pd
 from random import sample
@@ -10,15 +10,15 @@ import csv
 
 data_path_reviews = os.path.join(os.getcwd(), 'app/data/BackendReviews.csv')
 data_path_scores = os.path.join(os.getcwd(), 'app/data/Scores.csv')
-data_path_model = os.path.join(os.getcwd(), 'app/DistilBert')
+# data_path_model = os.path.join(os.getcwd(), 'app/DistilBert')
 
 with open(data_path_scores, newline='') as csv_file:
     reader = csv.DictReader(csv_file)
     rows = {(row['State'], row['County']): row['Score'] for row in reader}
 
 df = pd.read_csv(data_path_scores)
-dbModel = tf.keras.models.load_model(data_path_model)
-dbTokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+# dbModel = tf.keras.models.load_model(data_path_model)
+# dbTokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
 
 def get_reviews(county_name):
@@ -28,18 +28,18 @@ def get_reviews(county_name):
     return sample(reviews, min(len(reviews), 20))
 
 
-def score_compute(reviews, model, tokenizer):
-    if len(reviews) != 0:
-        batch = tokenizer(reviews, max_length=128, padding=True, truncation=True, return_tensors='tf')
-        outputs = model(batch)
-        predictions = tf.nn.softmax(outputs['logits'], axis=-1)
-        score_sum = 0
-        for i in predictions:
-            score_sum += (i[1] - i[0])
-        score = score_sum/len(reviews)
-        return float(score)
-    else:
-        return np.nan
+# def score_compute(reviews, model, tokenizer):
+#     if len(reviews) != 0:
+#         batch = tokenizer(reviews, max_length=128, padding=True, truncation=True, return_tensors='tf')
+#         outputs = model(batch)
+#         predictions = tf.nn.softmax(outputs['logits'], axis=-1)
+#         score_sum = 0
+#         for i in predictions:
+#             score_sum += (i[1] - i[0])
+#         score = score_sum/len(reviews)
+#         return float(score)
+#     else:
+#         return np.nan
 
 
 # def get_sentiment_score(state_name, county_name):
